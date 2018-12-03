003a966d152ab3860cd35dae78d4c661da2a6483
Nikita Chotii <chotii.nikita@yoctocode.com>
2018-12-03 17:55:13 +0200

    Updates

---
+++ precache-manifest.a109b4c76e068f1c96df02e4b26ab474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e88c41b1de363b1cdcfa",
+    "url": "/Sandbox-React-Contact-Manager/static/js/runtime~main.e88c41b1.js"
+  },
+  {
+    "revision": "bb26dfe031bb3ad5301b",
+    "url": "/Sandbox-React-Contact-Manager/static/js/main.bb26dfe0.chunk.js"
+  },
+  {
+    "revision": "eefaff7460b3497fd79e",
+    "url": "/Sandbox-React-Contact-Manager/static/js/1.eefaff74.chunk.js"
+  },
+  {
+    "revision": "bb26dfe031bb3ad5301b",
+    "url": "/Sandbox-React-Contact-Manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "eefaff7460b3497fd79e",
+    "url": "/Sandbox-React-Contact-Manager/static/css/1.b3eb0403.chunk.css"
+  },
+  {
+    "revision": "029cfb419786215d3b85e372d3cc8edb",
+    "url": "/Sandbox-React-Contact-Manager/index.html"
+  }
+];
