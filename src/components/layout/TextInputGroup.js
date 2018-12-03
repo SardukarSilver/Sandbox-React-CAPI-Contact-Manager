7437e8618c26ab43ac5a4f115781ef8178785b21
Nikita Chotii <chotii.nikita@yoctocode.com>
2018-12-03 10:28:44 +0200

    minnor changes

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
 const TextInputGroup = ({
   label,
@@ -17,8 +17,8 @@ const TextInputGroup = ({
       <input
         type={type}
         name={name}
-        className={classnames('form-control form-control-lg', {
-          'is-invalid': error
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
         })}
         placeholder={placeholder}
         value={value}
@@ -31,16 +31,16 @@ const TextInputGroup = ({
 
 TextInputGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  error: PropTypes.string.isRequired
+  error: PropTypes.string
 };
 
 TextInputGroup.defaultProps = {
-  type: 'text'
+  type: "text"
 };
 
 export default TextInputGroup;
